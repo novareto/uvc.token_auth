4a8f1a4cf13a2bca37217dc4bc16b3b33d72d44a
Souheil CHELFOUH <trollfot@gmail.com>
2018-11-05 20:48:43 +0100

    Adding package skeleton

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1dev1'
+
+setup(name='uvc.token_auth',
+      version=version,
+      description="",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['uvc'],
+      include_package_data=True,
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = uvcsite
+      """,
+      )
